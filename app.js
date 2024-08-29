3ff3624d3515a643ca98b40bcfc448a29c739488
nathqueiroz <qrznathalia@gmail.com>
2024-08-29 18:05:52 -0300

    Terceira e quarta aula da Imersão Dev com Gemini

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Rebeca Andrade");
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      // Concatena o HTML de cada resultado à string 'resultados'
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descrição}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML completo da lista de resultados à seção
+    section.innerHTML = resultados;
+  }
+
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descrição: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descrição: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal" // Substitua por um link real para a Beatriz Souza
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descrição: "Rebeca Andrade é uma ginasta brasileira que fez história ao conquistar a primeira medalha de ouro olímpica na ginástica artística individual geral para o Brasil. Sua versatilidade e determinação a tornaram um ícone do esporte.",
+        link: "hhttps://pt.wikipedia.org/wiki/Rebeca_Andrades://exemplo.com/rebeca-andrade" // Substitua por um link real para a Rebeca Andrade
+    }
+];
+
